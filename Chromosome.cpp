29ecfea38d77adb0a15ab569e2e1e20e056b1dc1
samreenzafer <samreen.jasvi@gmail.com>
2010-11-01 11:34:38 -0700

    GERMLINE modified to use variable window sizes for finding matches for NON-HAPLOID and NON-ROI cases

--- Chromosome.cpp
+++ Chromosome.cpp
@@ -1,63 +1,82 @@
-// Chromosome.h.haplotyped markers for a chromosome
+// Chromosome.cpp.haplotyped markers for a chromosome
 
-#ifndef CHROMOSOME_H
-#define CHROMOSOME_H
-
-
-#include "BasicDefinitions.h"
-#include "MarkerSet.h"
-#include <vector>
+#include "Chromosome.h"
+#include <iostream>
 using namespace std;
 
-class Chromosome
+
+// Chromosome(): default constructor
+Chromosome::Chromosome()
+{}
+
+MarkerSet * Chromosome::getMarkerSet()
 {
-public:
+	return chromosome[position_ms];
+}
 
-	// Chromosome(): default constructor
-	// Precondition: None.
-	// Postcondition: chromosome is empty. maxSets is 0.
-	Chromosome();
+MarkerSet * Chromosome::getMarkerSet(unsigned int pos)
+{
+	return chromosome[pos];
+}
+
+void Chromosome::clear()
+{
+	for ( size_t i = 0 ; i < chromosome.size() ; i++ ) { delete chromosome[i]; }
+	chromosome.clear();
+}
+
+// addMarkerSet(): adds a MarkerSet
+void Chromosome::addMarkerSet(MarkerSet * ms)
+{
+	chromosome.push_back(ms);
+}
+
+//overloaded addMarkerSet() : loads the entire marker data into a buffer chromosome
+void Chromosome::addMarkers(list<bool>* markers)
+{
+	buffer_chromosome = *markers;
+}
+
+void Chromosome::print_snps(ostream& out, unsigned int start, unsigned int end)
+{
+	unsigned int p_ms = position_ms;
+
+	unsigned int ms_start = start / MARKER_SET_SIZE;
+	unsigned int ms_end = end / MARKER_SET_SIZE;
+	if( start % MARKER_SET_SIZE != 0 ) { position_ms = ms_start; chromosome[ms_start++]->print(out,start % MARKER_SET_SIZE,MARKER_SET_SIZE); out << ' '; }
+	print(out,ms_start,ms_end);
+	if( end % MARKER_SET_SIZE != 0 ) { out << ' '; chromosome[ms_end]->print(out,0,end % MARKER_SET_SIZE); }
+
+	position_ms = p_ms;
+}
+
+void Chromosome::print(ostream& out,unsigned int start,unsigned int end)
+{
+	for(position_ms=start;position_ms<end;position_ms++) 
+	{
+		if( position_ms > start ) out << ' ';
+		chromosome[position_ms]->print(out);
+	}
+}
+
+ostream& operator<<(ostream &fout, Chromosome& c)
+{
+	fout << c.getMarkerSet();
+	return fout;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void Chromosome::updateMarkerSet(unsigned int start, unsigned int end)
+{
+	MarkerSet* ms = new MarkerSet(true); 
+	for(unsigned int i = start;  i<end; i++)
+	{
+		ms->pushback((i-start),buffer_chromosome.front());
+		buffer_chromosome.pop_front();				// future implementation for Overlapping windows needs non-modified buffer
+	}
 	
-	// getMarkerSet(): accessor for MarkerSet objects.
-	// Precondition: None.
-	// Postcondition: If index refers to an index for which memory has been
-	//   allocated, then the MarkerSet at position index has been returned;
-	//   otherwise a warning is printed and the default MarkerSet is returned.
-	MarkerSet* getMarkerSet();
-	MarkerSet* getMarkerSet(unsigned int);
+	chromosome.push_back(ms);
+}
 
-	// addMarkerSet(): adds a MarkerSet
-	// Precondition: None.
-	// Postcondition: If chromosome does not yet have maxSets MarkerSet objects, 
-	//  then ms is added to the end of chromosome; otherwise a warning is printed.
-	void addMarkerSet(MarkerSet * ms);
-	// addMarkers() : loads the entire marker data into a buffer chromosome
-	void addMarkers(list<bool>* markers);
-
-	void clear();
-	//void initialize();
-
-	void print(ostream& out,unsigned int,unsigned int);
-	void print_snps(ostream& out, unsigned int, unsigned int);
-
-
-	///////////////////////////////////////////////
-	void updateMarkerSet(unsigned int start, unsigned int end);
-
-private:
-
-	// Storage for chromosome MarkerSet objects
-	vector<MarkerSet * > chromosome;
-
-	//List of marker bits
-	list<bool> buffer_chromosome;
-
-	///////////////////////////////////
-	//vector<MarkerSet * > init_chromosome;
-};
-
-ostream &operator<<(ostream &fout, Chromosome&);
-
-#endif
-
-// end Chromosome.h
+// end Chromosome.cpp
